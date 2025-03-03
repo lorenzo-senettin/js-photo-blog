5ad93c34564eb475f6e1c22b91d412988c5e698b
lorenzo-senettin <senettin.lorenzo54@gmail.com>
2025-03-03 11:11:44 +0100

    edit card markup and move to js, edit css, add js api call, add card to html

---
+++ script.js
@@ -0,0 +1,45 @@
+// const for dom card-container
+const cardContainerEl = document.getElementById('card-container');
+
+// const for api
+const booleanEndPoint = "https://lanciweb.github.io/demo/api/pictures/";
+
+// call api
+fetch(booleanEndPoint)
+  .then(response => response.json())
+  .then(data => {
+    displayCards(data);
+  })
+  .catch(err => console.error(err));
+
+
+// create card markup
+function markup(url, date, place) {
+  return `
+    <div class="col-12 col-md-6 col-lg-4 d-flex justify-content-center">
+      <div class="card">
+        <div class="up-card">
+          <img class="pin" src="./assets/img/pin.svg" alt>
+          <div class="img">
+            <img class="image" src="${url}" alt>
+          </div>
+        </div>
+        <div class="down-card">
+          <p class="date text-muted">${date}</p>
+          <p class="place"><strong>${place}</strong></p>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+
+// add card to html
+function displayCards(data) {
+  data.forEach(item => {
+    const url = item.url;
+    const date = item.date;
+    const place = item.title;
+    cardContainerEl.innerHTML += markup(url, date, place);
+  });
+}
